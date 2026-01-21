daad95c86f125d17ebc9ff5b86935beea08e4d61
bestinh4 <diiogo49@gmail.com>
2026-01-21 05:35:10 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,9 @@
-// Inicializar Firebase
-var firebaseConfig = {
+// Importar funções do Firebase (v9 modular)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+// Configuração do Firebase
+const firebaseConfig = {
   apiKey: "AIzaSyADQZY0PLEcJUPjr2IxC5fjJmMgHXW3Eio",
   authDomain: "ousadiaapp.firebaseapp.com",
   projectId: "ousadiaapp",
@@ -7,37 +11,37 @@ var firebaseConfig = {
   messagingSenderId: "736839415891",
   appId: "1:736839415891:web:9335932f5aa4a5689a6ea6"
 };
-firebase.initializeApp(firebaseConfig);
 
-// Referência de autenticação
-var auth = firebase.auth();
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 // Login com Google
-function loginGoogle() {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  auth.signInWithPopup(provider)
-    .then(function(result) {
+export function loginGoogle() {
+  signInWithPopup(auth, provider)
+    .then((result) => {
       console.log("Usuário logado:", result.user.displayName);
       window.location.href = 'dashboard.html'; // Redireciona após login
     })
-    .catch(function(error) {
+    .catch((error) => {
       alert("Erro ao logar: " + error.message);
     });
 }
 
 // Logout
-function logout() {
-  auth.signOut()
-    .then(function() {
-      window.location.href = 'index.html'; // Redireciona após logout
+export function logout() {
+  signOut(auth)
+    .then(() => {
+      window.location.href = 'index.html';
     })
-    .catch(function(error) {
+    .catch((error) => {
       alert("Erro ao sair: " + error.message);
     });
 }
 
 // Monitorar autenticação
-auth.onAuthStateChanged(function(user) {
+onAuthStateChanged(auth, (user) => {
   if (user) {
     console.log("Usuário ativo:", user.displayName);
   } else {
