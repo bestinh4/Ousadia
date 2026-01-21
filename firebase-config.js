c2102cca2c051c051849cc655ccc3f9354ae54e0
bestinh4 <diiogo49@gmail.com>
2026-01-21 05:04:15 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyADQZY0PLEcJUPjr2IxC5fjJmMgHXW3Eio",
+  authDomain: "ousadiaapp.firebaseapp.com",
+  projectId: "ousadiaapp",
+  storageBucket: "ousadiaapp.firebasestorage.app",
+  messagingSenderId: "736839415891",
+  appId: "1:736839415891:web:9335932f5aa4a5689a6ea6"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
