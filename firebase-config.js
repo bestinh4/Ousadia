e4ea8327e7cd283f82665b975fcfd20d1987d76f
bestinh4 <diiogo49@gmail.com>
2026-01-21 07:21:06 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // Firebase v9 modular
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, updateDoc, doc, query, where } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyADQZY0PLEcJUPjr2IxC5fjJmMgHXW3Eio",
@@ -12,27 +12,8 @@ const firebaseConfig = {
   appId: "1:736839415891:web:9335932f5aa4a5689a6ea6"
 };
 
+// Inicializa Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-
-// Login Google
-export async function loginGoogle() {
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    console.log("Usuário logado:", user.displayName);
-    // Redireciona para dashboard
-    window.location.href = "dashboard.html";
-  } catch (error) {
-    console.error("Erro login:", error);
-  }
-}
-
-// Logout
-export function logout() {
-  signOut(auth).then(() => {
-    window.location.href = "index.html";
-  });
-}
